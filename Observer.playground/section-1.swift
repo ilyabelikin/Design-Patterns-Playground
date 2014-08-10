3324c8b1c52b40cf36b85928d7539abd8c05b80f
Ilya Belikin <forihrd@gmail.com>
2014-08-10 16:29:52 +0800

    update Observer with example

--- Observer.playground/section-1.swift
+++ Observer.playground/section-1.swift
@@ -1,13 +1,14 @@
 //
 // TODO: Motivation
 //
+// Real-world analogy is newspaper subscriprion.
+//
 // # Definition
 //
 // The Observer pattern defines a one-to-many dependency between objects
 // so that one object change state, all of it dependent are notified and
 // can be updated automatically.
 //
-
 // TODO: # Wether station example (basic design)
 
 protocol Observable {
@@ -18,14 +19,27 @@ protocol Observable {
 }
 
 protocol Observer {
-    func update (updatedObject: Observable)
+    // FIXIT: in beta5, serviceKit terminated on desired declaration
+    // func update (updatedObject: Observable)
+    func update (updatedObject: AnyObject)
+}
+
+enum MesureType {
+    case Humidity, Temperature, Pressure
+}
+
+struct Mesures {
+    var data = [MesureType: Double]()
 }
 
 class WetherStation: Observable {
    
-    var humidity: Double?
-    var temperature: Double?
-    var pressure: Double?
+    var mesures : Mesures = Mesures() {
+        didSet {
+            println("New data arrived.\n")
+            self.notifyObservers()
+        }
+    }
     
     var observers = [Observer]()
     
@@ -34,11 +48,22 @@ class WetherStation: Observable {
     }
     
     func removeObserver(observer: Observer) {
-
+        var checkedObservers = [Observer]()
+        for o in observers {
+            // TODO: Implement remove
+            // Error: Observer is not convertble to UInt8.
+            // Wow. I don't understand why is that.
+            // if o != observer {
+                checkedObservers.append(o)
+            // }
+        }
+        observers = checkedObservers
     }
     
     func notifyObservers() {
-        
+        for observer in observers {
+            observer.update(self)
+        }
     }
 
 }
@@ -48,32 +73,90 @@ protocol Display {
 }
 
 class CurrentConditionDisplay: Observer, Display {
-    func update (updatedObject: Observable) {
+    var temperature: Double?
+    
+    func update (updatedObject: AnyObject) {
+        if let wetherStation = updatedObject as? WetherStation {
+            self.temperature = wetherStation.mesures.data[.Temperature] ?? nil
+        }
         
+        display()
     }
     
     func display() {
-        
+        println("--- Current ---")
+        if let temp = self.temperature {
+            println("Temeperature is \(temp)˚C")
+        } else {
+            println("We have no idea what is goinig on outside.")
+        }
+        println()
     }
 }
 
 
 class ForecastDisplay: Observer, Display {
-    func update (updatedObject: Observable) {
-        
+    func update (updatedObject: AnyObject) {
+        display()
     }
     
     func display() {
-        
+        println("--- Forecast ---")
+        // Sorry, just random stuff
+        println("Everything is awesome! Everything is good if you part of the heap. Everything is awes-o-o-ome! Till you have a li-i-ink.\n")
     }
 }
 
+import Foundation
+
 class WetherStatisticDisplay: Observer, Display {
-    func update (updatedObject: Observable) {
+    var data = [NSTimeInterval: Mesures]()
+    
+    func update (updatedObject: AnyObject) {
+        if let wetherStation = updatedObject as? WetherStation {
+            let timeStamp = NSDate().timeIntervalSinceReferenceDate
+            data[timeStamp] = wetherStation.mesures
+        }
         
+        display()
+    }
+    
+    func avarageTemperature () -> Double {
+        
+        var numberOfMesures = 0
+        var sum = 0.0
+        
+        for (_, value) in data {
+            if let temp = value.data[MesureType.Temperature] {
+                sum += temp
+                numberOfMesures++
+            }
+        }
+        return sum / Double(numberOfMesures)
     }
     
     func display() {
-        
+        println("--- Staticstics ---")
+        println("Avarage temperature for all time is \(avarageTemperature())\n")
     }
 }
+
+let wetherStation = WetherStation()
+let current = CurrentConditionDisplay()
+let stat = WetherStatisticDisplay()
+let forecast = ForecastDisplay()
+
+current.display()
+
+wetherStation.registerObserver(current)
+wetherStation.registerObserver(stat)
+wetherStation.registerObserver(forecast)
+
+wetherStation.mesures = Mesures(data: [.Humidity : 32.8, .Temperature : 34.4 , .Pressure : 80.0 ])
+
+wetherStation.mesures = Mesures(data: [.Humidity : 52.8, .Temperature : 38.7 , .Pressure : 50.0 ])
+
+
+wetherStation.mesures = Mesures(data: [.Humidity : 70.1, .Temperature : 30.1 , .Pressure : 89.0 ])
+
+wetherStation.notifyObservers()
